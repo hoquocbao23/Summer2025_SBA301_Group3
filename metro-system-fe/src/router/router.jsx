5c9cbdefe68b3b44685c1c72740485b579e5f29d
hoquocbao23 <hoquocbao6123@gmail.com>
2025-05-27 16:22:36 +0700

    new dashboard

--- metro-system-fe/src/router/router.jsx
+++ metro-system-fe/src/router/router.jsx
@@ -2,8 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/home";
 import TicketSearchOverview from "../components/ticket-search/TicketSearchOverview";
-import PaymentMethodList from "../components/passenger/PaymentMethodList";
+
 import PassengerPage from "../pages/passenger/passenger-page";
+import DashboardLayout from "../pages/dashboard/dashboard-layout";
+import CustomerDashboard from "../pages/dashboard/customer/customer-dashboard";
+import PromotionDashboard from "../pages/dashboard/promotion/promotion-dashboard";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -14,12 +17,26 @@ const router = createBrowserRouter([
                 element: <Home/>
             },
             {
-                path: "/tickets",
+                path: "tickets",
                 element: <TicketSearchOverview/>
             },
             {
-                path: "/passenger",
+                path: "passenger",
                 element: <PassengerPage/>
+            }
+        ]
+    },
+    {
+        path: "/dashboard",
+        element: <DashboardLayout/>,
+        children: [
+            {
+                path: "customers",
+                element: <CustomerDashboard/>
+            },
+            {
+                path: "promotions",
+                element: <PromotionDashboard/>
             }
         ]
     }
