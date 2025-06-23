c1057bca6e295404352a80fa8ef29306452e02c6
hoquocbao23 <hoquocbao6123@gmail.com>
2025-06-23 09:00:49 +0700

    checkin checkout

---
+++ metro-system-fe/src/services/ticket.js
@@ -0,0 +1,36 @@
+import axiosInstance from '../config/axios';
+
+const TICKET_ENDPOINTS = {
+  TICKETS: '/tickets'
+};
+
+export default function useTicket() {
+  const getTicketById = async (ticketId) => {
+    try {
+      const response = await axiosInstance.get(`${TICKET_ENDPOINTS.TICKETS}/${ticketId}`);
+      return response.data.data;
+    } catch (error) {
+      throw error;  
+    }
+  };
+
+  const checkInTicket = async (ticketId) => {
+    try {
+      const response = await axiosInstance.post(`${TICKET_ENDPOINTS.TICKETS}/check-in?ticketId=${ticketId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const checkOutTicket = async (ticketId) => {
+    try {
+      const response = await axiosInstance.post(`${TICKET_ENDPOINTS.TICKETS}/check-out?ticketId=${ticketId}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return { getTicketById, checkInTicket, checkOutTicket };
+}
