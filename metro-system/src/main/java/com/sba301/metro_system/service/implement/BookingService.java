e3290112917f507fd0c28efb419a7cc890d63328
hoquocbao23 <hoquocbao6123@gmail.com>
2025-07-21 04:20:54 +0700

    update promotion

--- metro-system/src/main/java/com/sba301/metro_system/service/implement/BookingService.java
+++ metro-system/src/main/java/com/sba301/metro_system/service/implement/BookingService.java
@@ -1,13 +1,19 @@
 package com.sba301.metro_system.service.implement;
 
 import com.sba301.metro_system.dto.request.route.RouteRequest;
+import com.sba301.metro_system.dto.response.BookingResponseDto;
+import com.sba301.metro_system.dto.response.TicketResponseDto;
+import com.sba301.metro_system.entity.Account;
 import com.sba301.metro_system.entity.Booking;
+import com.sba301.metro_system.mapper.BookingMapper;
 import com.sba301.metro_system.repository.BookingRepository;
 import com.sba301.metro_system.service.IBookingService;
+import com.sba301.metro_system.utils.AccountHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +25,8 @@ public class BookingService implements IBookingService {
     public void createBooking(Booking booking) {
         bookingRepository.save(booking);
     }
+
+
 
     public Booking getBookingById(long bookingId) {
         return bookingRepository.findById(bookingId).get();
